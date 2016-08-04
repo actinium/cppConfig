3c409aa80a2d40b21277cdbddf879a376814e315
actinium <andreas.cederholm@gmail.com>
2016-08-05 01:38:28 +0200

    Change integral_constant<bool> to true_type and false_type.

--- src/config_traits.h
+++ src/config_traits.h
@@ -1,21 +1,21 @@
 #include <type_traits>
 
 template<typename T>
-struct is_config_type:std::integral_constant<bool,false>{};
+struct is_config_type: std::false_type{};
 
-template<> struct is_config_type <bool>:               std::integral_constant<bool,true>{};
+template<> struct is_config_type <bool>:               std::true_type{};
 
-template<> struct is_config_type <int>:                std::integral_constant<bool,true>{};
-template<> struct is_config_type <long>:               std::integral_constant<bool,true>{};
-template<> struct is_config_type <long long>:          std::integral_constant<bool,true>{};
+template<> struct is_config_type <int>:                std::true_type{};
+template<> struct is_config_type <long>:               std::true_type{};
+template<> struct is_config_type <long long>:          std::true_type{};
 
-template<> struct is_config_type <unsigned int>:       std::integral_constant<bool,true>{};
-template<> struct is_config_type <unsigned long>:      std::integral_constant<bool,true>{};
-template<> struct is_config_type <unsigned long long>: std::integral_constant<bool,true>{};
+template<> struct is_config_type <unsigned int>:       std::true_type{};
+template<> struct is_config_type <unsigned long>:      std::true_type{};
+template<> struct is_config_type <unsigned long long>: std::true_type{};
 
-template<> struct is_config_type <float>:              std::integral_constant<bool,true>{};
-template<> struct is_config_type <double>:             std::integral_constant<bool,true>{};
-template<> struct is_config_type <long double>:        std::integral_constant<bool,true>{};
+template<> struct is_config_type <float>:              std::true_type{};
+template<> struct is_config_type <double>:             std::true_type{};
+template<> struct is_config_type <long double>:        std::true_type{};
 
-template<> struct is_config_type <std::string>:        std::integral_constant<bool,true>{};
+template<> struct is_config_type <std::string>:        std::true_type{};
 
